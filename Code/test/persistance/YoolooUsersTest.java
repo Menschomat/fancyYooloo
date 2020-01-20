a2a50a59ec470dd115fa12aeb29d584cc0389550
Lukas Luzius <lwluzius@gmail.com>
2020-01-20 13:20:22 +0100

    JUnits

--- Code/test/persistance/YoolooUsersTest.java
+++ Code/test/persistance/YoolooUsersTest.java
@@ -1,11 +1,15 @@
 package persistance;
 
 import common.YoolooKarte;
+import common.YoolooKartenspiel;
 import common.YoolooSpieler;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,11 @@ class YoolooUsersTest {
 
     @BeforeEach
     void init() {
+        users = new YoolooUsers();
+    }
+
+    @AfterEach
+    void cleanup() {
         File file = new File("users.data");
         if (file.exists()) {
             file.delete();
@@ -22,9 +31,22 @@ class YoolooUsersTest {
     }
 
     @Test
-    void test() {
+    void userNotExistantTest() {
         YoolooSpieler spieler = new YoolooSpieler("test", 10);
-        assertEquals(new YoolooKarte[10], users.getUserCardOrder(spieler));
+        YoolooKarte[] ref = new YoolooKarte[10];
+        assertEquals(ref.length, users.getUserCardOrder(spieler).length);
+        assertEquals(ref[0], users.getUserCardOrder(spieler)[0]);
+    }
+
+    @Test
+    void addUserTest() {
+        YoolooSpieler spieler = new YoolooSpieler("test", 1);
+        YoolooKarte[] ref = {new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Blau, 99)};
+        spieler.setAktuelleSortierung(ref);
+        users.setUserCardOrder("test", new ArrayList<>(Arrays.asList(99)));
+        assertEquals(ref.length, users.getUserCardOrder(spieler).length);
+        assertEquals(ref[0].getWert(), users.getUserCardOrder(spieler)[0].getWert());
+        assertEquals(ref[0].getFarbe(), users.getUserCardOrder(spieler)[0].getFarbe());
     }
 
 }
