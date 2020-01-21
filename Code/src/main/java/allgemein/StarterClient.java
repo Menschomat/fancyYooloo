f31a0865d44f956be26c71dde05db9913ed6f416
Falco Weihrauch <falco@robaux.de>
2020-01-21 20:53:22 +0100

    added properties for client

--- Code/src/main/java/allgemein/StarterClient.java
+++ Code/src/main/java/allgemein/StarterClient.java
@@ -5,16 +5,18 @@
 package allgemein;
 
 import client.YoolooClient;
+import utils.PropertiesController;
+
+import java.util.Properties;
 
 public class StarterClient {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+		Properties props = PropertiesController.getProperties("client");
+        // Starte Client
+        String hostname = props.get("connection.server.hostname") != null ? props.get("connection.server.hostname").toString() : "localhost";
+        int port = props.get("connection.server.port") != null ? Integer.parseInt(props.get("connection.server.port").toString()) : 44137;
+        YoolooClient client = new YoolooClient(hostname, port);
+        client.startClient();
 
-		// Starte Client
-		String hostname = "localhost";
-//		String hostname = "10.101.251.247";
-		int port = 44137;
-		YoolooClient client = new YoolooClient(hostname, port);
-		client.startClient();
-
-	}
+    }
 }
--- Code/src/main/java/utils/PropertiesController.java
+++ Code/src/main/java/utils/PropertiesController.java
@@ -9,19 +9,19 @@ import java.util.Properties;
 public abstract class PropertiesController {
     private static final ClassLoader classLoader = PropertiesController.class.getClassLoader();
     private static final Properties properties = new Properties();
-    public static Properties getProperties() {
-        String path = "./application.properties";
+
+    public static Properties getProperties(String name) {
+        String path = "./" + name + ".properties";
         try {
             InputStream fis = new FileInputStream(path);
             properties.load(fis);
         } catch (IOException e) {
-            try (InputStream input = Objects.requireNonNull(classLoader.getResource("application.properties")).openStream()) {
+            try (InputStream input = Objects.requireNonNull(classLoader.getResource(name + ".properties")).openStream()) {
                 properties.load(input);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
-
         return properties;
     }
 }
