29b52fe23d8c0db5f44a559f22c20b01752c1b04
pgahlen <pgahlen+github@gmail.com>
2020-01-22 10:06:06 +0100

    Updated Regeleinhaltung

--- Code/test/server/YoolooSessionTest.java
+++ Code/test/server/YoolooSessionTest.java
@@ -9,9 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class YoolooSessionTest {
     @Test
     public void testDuplicateCard() {
-        YoolooSession session = new YoolooSession(1, YoolooServer.GameMode.GAMEMODE_NULL, null);
+        YoolooSession session = new YoolooSession(2, YoolooServer.GameMode.GAMEMODE_SINGLE_GAME, null);
 
         session.spieleKarteAus(0, 0, new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Blau, 1));
+        session.spieleKarteAus(0, 1, new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Blau, 2));
         session.stichFuerRundeAuswerten(0);
         assertEquals(session.spieleKarteAus(1, 0, new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Blau, 1)), false);
     }
