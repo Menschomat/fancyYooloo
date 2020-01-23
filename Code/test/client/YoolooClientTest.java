fc2bad0d6b954620c2add8a477e79530601fd34b
Loenser <lukas.loenser@t-online.de>
2020-01-23 10:21:42 +0100

    Test für die Sortierung und Dateien die es nicht geschafft haben zur Anschauung

--- Code/test/client/YoolooClientTest.java
+++ Code/test/client/YoolooClientTest.java
@@ -1,18 +1,48 @@
 package client;
 
+import common.YoolooKarte;
+import common.YoolooKartenspiel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class YoolooClientTest {
+    static YoolooClient yoolooClient;
+
+    @BeforeAll
+    static void createData(){
+        yoolooClient = new YoolooClient();
+        yoolooClient.createSpieler();
+
+    }
+
 
     @Test
-    void startClient() {
-        assertEquals(true, false);
+    void testFancySortierungKleinGroß(){
+        YoolooKarte[] sortierung = yoolooClient.fancySortierung("testClientKleinGroß");
+        YoolooKarte[] erwarteteSortierung = new YoolooKarte[10];
+        for(int i = 0; i < 10; i ++){
+            YoolooKarte yoolooKarte = new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Rot, i + 1);
+            erwarteteSortierung[i] = yoolooKarte;
+        }
+        for(int i = 0; i < 10; i++) {
+            assertEquals(erwarteteSortierung[i].getWert(), sortierung[i].getWert());
+            assertEquals(erwarteteSortierung[i].getFarbe(), sortierung[i].getFarbe());
+        }
     }
 
     @Test
-    void ausgabeKartenSet() {
-        assertEquals(true, false);
+    void testFancySortierungGroßKlein(){
+        YoolooKarte[] sortierung = yoolooClient.fancySortierung("testClientGroßKlein");
+        YoolooKarte[] erwarteteSortierung = new YoolooKarte[10];
+        for(int i = 0; i < 10; i ++){
+            YoolooKarte yoolooKarte = new YoolooKarte(YoolooKartenspiel.Kartenfarbe.Rot, sortierung.length - i);
+            erwarteteSortierung[i] = yoolooKarte;
+        }
+        for(int i = 0; i < 10; i++) {
+            assertEquals(erwarteteSortierung[i].getWert(), sortierung[i].getWert());
+            assertEquals(erwarteteSortierung[i].getFarbe(), sortierung[i].getFarbe());
+        }
     }
 }
